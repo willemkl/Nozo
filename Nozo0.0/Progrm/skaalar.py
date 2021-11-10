17a3a146a96afd27d68d7bc9bcfdc26acb60a8a7
willemkl <92948497+willemkl@users.noreply.github.com>
2021-11-10 22:46:06 +0200

    Delete Nozo0.0 directory

--- Nozo0.0/Progrm/skaalar.py
+++
@@ -1,6 +0,0 @@
-def skaalar(maatriks,skaalar):
-    # korrutamine skaalariga
-    for i in range(len(maatriks)):
-        for j in range(len(maatriks[0])):
-            maatriks[i][j] = maatriks[i][j]*skaalar
-    return maatriks
--- Nozo0.0/Progrm/väljund.py
+++
@@ -1,5 +0,0 @@
-def väljund(maatriks):
-    for rida in maatriks:
-        for veerg in rida:
-            print(veerg, end = " ")
-        print()
