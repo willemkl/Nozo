9b107cdb9899bb328b69c468e66b8c472a1e1d61
willemkl <92948497+willemkl@users.noreply.github.com>
2021-11-11 18:13:06 +0200

    Add files via upload
    uus ja parem lõng_list
    nüüd muudab ka arve, mitte vaid numbreid

---
+++ Nozo0.0/Progrm/lõng_list2.py
@@ -0,0 +1,28 @@
+# muudab järjendi maatriksiks(listiks)
+def lõng_list(lõng):
+    numbrid_miinus = "-0123456789"
+    arv = ""
+    rida = []
+    maatriks = []
+    # loeb järjendi läbi
+    for i in range(len(lõng)):
+        # "[" ei ole vaja vaadata
+        if lõng[i] == "[":
+            continue
+        else:
+            # loome arvu
+            if lõng[i] in numbrid_miinus:
+                arv = arv + lõng[i]
+            # asetame arvu ritta
+            elif lõng[i] == "," and lõng[i+1] != "[":
+                rida.append(int(arv))
+                arv = ""
+            # asetame arvu ritta ja rea maatriksisse
+            if lõng[i] == "]" and lõng[i-1] != "]":
+                rida.append(int(arv))
+                arv = ""
+                maatriks.append(rida)
+                rida = []
+    return maatriks
+        
+print(lõng_list("[[12,-34,3],[1,3,-1000]]"))
