bb8221da1ae211985ff4a86a0cb714f543026183
willemkl <92948497+willemkl@users.noreply.github.com>
2021-12-15 22:08:09 +0200

    Delete Nozo0.0 directory

--- Nozo0.0/Progrm/skalaar.py
+++
@@ -1,6 +0,0 @@
-def skalaar(maatriks,skalaar):
-    # korrutamine skalaariga
-    for i in range(len(maatriks)):
-        for j in range(len(maatriks[0])):
-            maatriks[i][j] = maatriks[i][j]*skalaar
-    return maatriks
